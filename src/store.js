7d7baabf1c5544b508955b61c6469be25dd65475
Andrew <andy.birks@gmail.com>
2019-06-03 14:51:20 +0100

    Formatting

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from './reducers/index';
+import rootReducer from "./reducers/index";
 
 const initialState = {};
 
@@ -12,7 +12,6 @@ const store = createStore(
   compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
   )
 );
 
